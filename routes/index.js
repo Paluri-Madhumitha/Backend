2b3fc79bae51313ad21097cd6bf563cce6388571
Paluri-Madhumitha <madhumithapaluri2004@gmail.com>
2025-03-01 17:56:32 +0530

    Add files via upload

---
+++ routes/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+  campaign: require("../controllers/campaign"),
+  user: require("../controllers/user"),
+  payment: require("../controllers/payment"),
+  donation: require("../controllers/donation"),
+  query: require("../controllers/query"),
+};
