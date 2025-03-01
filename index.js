45d59ca2a8081c89d31176a58ead0689925216ce
Paluri-Madhumitha <madhumithapaluri2004@gmail.com>
2025-03-01 18:05:55 +0530

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  campaign: require("../controllers/campaign"),
-  user: require("../controllers/user"),
-  payment: require("../controllers/payment"),
-  donation: require("../controllers/donation"),
-  query: require("../controllers/query"),
-};
